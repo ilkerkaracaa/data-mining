4f41732970eca6fc7dc72810352ac97c0352e8cb
İlker Karaca <ikaracadeveloper@gmail.com>
2023-10-29 17:40:06 +0300

    numpy learning

--- example-2.py
+++ example-2.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import openpyxl
 
-file_name = 'FIM_Demo3.xlsx'
-wb = openpyxl.load_workbook(file_name)
+file_name = "FIM_Demo3.xlsx"
+wb = openpyxl.load_workbook(filename=file_name, read_only=True)
 sheet = wb[wb.sheetnames[0]]
 
 database = np.array(pd.DataFrame(sheet.values))
@@ -25,17 +25,16 @@ numOfUniqeItems = len(uniqeItems)
 numOfTransactions = database.shape[0]
 
 dataMatrix = np.zeros((numOfTransactions, numOfUniqeItems))
-for i in range (0,database.shape[0]):
-    transaction = database[i,:]
+for i in range(0, database.shape[0]):
+    transaction = database[i, :]
     for item in transaction:
         if item != None:
             I = uniqeItems.index((item))
             dataMatrix[i][I] = 1
-            
+
 counts = np.sum(dataMatrix, axis=0)
 minSup = 4
 I = np.nonzero(counts >= minSup)[0]
 uniqeItems = [uniqeItems[i] for i in I]
-dataMatrix = dataMatrix[:,I]
+dataMatrix = dataMatrix[:, I]
 counts = counts[I]
-
