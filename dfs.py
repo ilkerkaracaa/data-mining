acc2d28eccf728f31bfb691d0f92e9c8e92924c2
İlker Karaca <ikaracadeveloper@gmail.com>
2023-11-08 10:21:27 +0300

    dfs implementation in python

---
+++ dfs.py
@@ -0,0 +1,13 @@
+R = ["a", "b", "c", "d", "e"]
+
+
+def dfsLoop(item, R):
+    E = R[R.index(item[-1]) + 1 :]
+    for suffix in E:
+        print(item + suffix)
+        dfsLoop(item + suffix, E)
+
+
+for item in R:
+    print(item)
+    dfsLoop(item, R)
